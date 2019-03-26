eb380fc2ddfc4542a55f8681ab6f8ebfd3d22781
hernachu <opihernayanti00@gmail.com>
2019-03-26 22:09:06 +0700

    first commit

--- index.php
+++ index.php
@@ -10,6 +10,35 @@ require 'koneksi.php';
 </head>
 <body style="background: url(4.jpg);">
 
+
+  <form id="form-container" class="form-container">
+   
+<!--Element-elemen tag <body> tulis disini-->
+<header> <!--Section HEADER-->
+
+<center>
+       <div class="menu">
+     
+      <ul>
+        
+      
+        <label for="input">Wikipedia :</label>
+        <input type="text" id="input" value="">
+        <button id="submit-btn">submit</button>
+          
+      </ul>
+      
+    </div>
+    </center>
+  </header>
+
+</header>
+ <li class="wikipedia-container">
+        <h3 id="wikipedia-header">Relevant Wikipedia Links</h3>
+        <ul id="wikipedia-links">Type in an address above and find relevant Wikipedia articles here!</ul>
+    </li>
+
+
 <center>
   <br>
     <h2>Daftar Program Studi Strata 1 Fakultas Ilmu Komputer</h2>
@@ -42,5 +71,11 @@ require 'koneksi.php';
 <br>
 <button><a href="register.php">Login</a></button>
 </center>
+
+<script src="jquery.min.js"></script>
+    <script src="script.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/docs.min.js"></script>
+
   </body>
 </html>
